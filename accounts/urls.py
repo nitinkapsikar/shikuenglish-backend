343b973c18ff91ac8cb568afe352e7df62d1e516
purushottam kapsikar <kapsikarpk@gmail.com>
2026-06-01 09:19:06 +0530

    Removed LessonChatAPIView

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from .views import (SendOTPView, VerifyOTPView, ChatAPIView,
-                    LessonChatAPIView,LessonAPIView,UserProgressAPIView,ActivatePremiumAPIView)
+                    LessonAPIView,UserProgressAPIView,ActivatePremiumAPIView)
 
 urlpatterns = [
     path('send-otp/', SendOTPView.as_view(), name="send-otp"),
     path('verify-otp/', VerifyOTPView.as_view(), name="verify-otp"),
     path('chat/', ChatAPIView.as_view()),
-    path("lesson/chat/", LessonChatAPIView.as_view()),
+
     path("lesson/", LessonAPIView.as_view()),
     path(
         "progress/",
